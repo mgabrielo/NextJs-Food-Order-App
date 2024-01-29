00735443b112a672131d7a059f1bbc00ce47fca3
Scott Nitro <scottnitro1@gmail.com>
2024-01-29 13:09:50 +0000

    2nd commit

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -5,26 +5,25 @@ import Image from "next/image";
 import { redirect } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import EditProfile from "../../components/icons/EditProfile";
+
 export default function ProfilePage() {
   const session = useSession();
-  const { status, update } = useSession();
+  const { data, status, update } = useSession();
   const [userName, setUserName] = useState("");
   const [saved, setSaved] = useState(false);
   const [isSaving, setIsSaving] = useState(false);
   const [image, setImage] = useState("");
+
   useEffect(() => {
     if (status == "authenticated") {
       const usernameExtract =
-        session?.data?.user?.name !== undefined
-          ? session?.data?.user?.name
-          : session?.data?.user?.email.substring(
-              0,
-              session?.data?.user?.email.indexOf("@")
-            );
+        data?.user?.name !== undefined
+          ? data?.user?.name
+          : data?.user?.email.substring(0, data?.user?.email.indexOf("@"));
       setUserName(usernameExtract);
       const profileImgExtract =
-        session?.data?.user?.image !== undefined
-          ? session?.data?.user?.image
+        data?.user?.image !== undefined
+          ? data?.user?.image
           : "https://cdn.pixabay.com/photo/2017/11/10/05/48/user-2935527_1280.png";
       setImage(profileImgExtract);
     }
@@ -33,10 +32,11 @@ export default function ProfilePage() {
         setSaved(false);
       }, 2500);
   }, [session, status, saved]);
-  if (session?.status === "loading") {
+
+  if (status === "loading") {
     return <p>Loading...</p>;
   }
-  if (session?.status === "unauthenticated") {
+  if (status === "unauthenticated") {
     return redirect("/login");
   }
 
@@ -78,53 +78,47 @@ export default function ProfilePage() {
       <h1 className="text-primary text-center text-2xl font-semibold mb-4">
         Profile
       </h1>
-      <form className="max-w-xl mx-auto">
+      <form className="w-full md:max-w-xl mx-auto">
         {saved && (
           <h2 className="text-center bg-green-200 border border-green-700 p-3 rounded-lg text-green-800 my-5">
             Profile Saved
           </h2>
         )}
-        <div className="flex gap-2 items-center">
-          <div>
-            <div className="rounded-md relative justify-center">
-              {image && (
+        <div className="flex flex-col md:flex-row lg:flex-row gap-2 items-center">
+          <div className="relative justify-center items-center">
+            {image && (
+              <div className="relative w-[150px] h-[150px] object-contain justify-center items-center my-2">
                 <Image
                   src={image}
-                  width={100}
-                  height={100}
+                  fill
                   priority={true}
-                  style={{ objectFit: "contain" }}
                   alt={"user-icon"}
                   className="rounded-full w-auto h-auto items-center px-2 mb-4"
                 />
-              )}
-              <label>
-                <div className="flex justify-center items-center">
-                  <input
-                    type="file"
-                    className="hidden"
-                    onChange={(e) => handleFileChange(e)}
-                  />
-                  <span className="flex gap-2 border border-primary px-4 py-2 bg-primary text-white rounded-md">
-                    <EditProfile />
-                    <p>Edit</p>
-                  </span>
-                </div>
-              </label>
-            </div>
+              </div>
+            )}
+            <label>
+              <div className="flex justify-center items-center">
+                <input
+                  type="file"
+                  className="hidden"
+                  onChange={(e) => handleFileChange(e)}
+                />
+                <span className="flex gap-2 border border-primary px-4 py-2 bg-primary text-white rounded-md">
+                  <EditProfile />
+                  <p>Edit</p>
+                </span>
+              </div>
+            </label>
           </div>
-          <div className="space-y-5 grow">
+          <div className="space-y-5 w-full md:grow lg:grow px-5 md:px-0 lg:px-0">
             <input
               type="text"
               value={userName}
               onChange={(e) => setUserName(e.target.value)}
               disabled={isSaving}
             />
-            <input
-              type="email"
-              value={session?.data?.user?.email}
-              disabled={true}
-            />
+            <input type="email" value={data?.user?.email} disabled={true} />
             <button onClick={(e) => handleProfileUpdate(e)} disabled={isSaving}>
               {isSaving ? "Saving Update..." : "Submit"}
             </button>
